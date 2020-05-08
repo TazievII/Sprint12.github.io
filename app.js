a7d41238a0657276510740192a78c383959b874c
Ilya Taziev <tazievii@gmail.com>
2020-05-08 10:24:19 +0500

    Роуты

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const routes = require('./routes.js');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use('/', routes);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
