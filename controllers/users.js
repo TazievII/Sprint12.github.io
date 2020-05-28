8164921ace8ac2e0f638f188606a768be2ff0470
Ilya Taziev <tazievii@gmail.com>
2020-05-28 10:20:25 +0500

    Переработан контроллер создания пользователя

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const validator = require('validator');
 const User = require('../models/user');
 
@@ -10,8 +11,17 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
     .then((user) => {
       if (validator.isURL(avatar)) {
         res.send({ data: user });
