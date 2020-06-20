aaa98d6503ed16691d2b1f3180dd659d553bceef
Ilya Taziev <tazievii@gmail.com>
2020-06-20 20:37:15 +0500

    Joi валидация роутов user

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,23 @@
 const usersRouter = require('express').Router();
+const { Joi, celebrate } = require('celebrate');
 const {
   getUsers, findUser, updateUser, updateUserAvatar,
 } = require('../controllers/users');
 
+
 usersRouter.get('/', getUsers);
 usersRouter.get('/:id', findUser);
-usersRouter.patch('/me', updateUser);
-usersRouter.patch('/me/avatar', updateUserAvatar);
+usersRouter.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateUser);
+usersRouter.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    // eslint-disable-next-line no-useless-escape
+    avatar: Joi.string().pattern(/^(?:([a-z]+):(?:([a-z]*):)?\/\/)?(?:([^:@]*)(?::([^:@]*))?@)?((?:[a-z0-9_-]+\.)+[a-z]{2,}|localhost|(?:(?:[01]?\d\d?|2[0-4]\d|25[0-5])\.){3}(?:(?:[01]?\d\d?|2[0-4]\d|25[0-5])))(?::(\d+))?(?:([^:\?\#]+))?(?:\?([^\#]+))?(?:\#([^\s]+))?$/).required(),
+  }),
+}), updateUserAvatar);
 
 module.exports = usersRouter;
